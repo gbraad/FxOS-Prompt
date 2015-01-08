0069caee78b33923c098486d001e17d9fc642f29
Gerard Braad <me@gbraad.nl>
2015-01-09 00:00:03 +0800

    Very crude monkeypatch solution to show last console.log

---
+++ js/console.js
@@ -0,0 +1,9 @@
+var real_console_log = console.log;
+
+// Overwrite the default log behavior to pipe to an output element.
+console.log = function(msg) {     
+  var output = document.getElementById("prompt-output");
+  if(output)
+    output.innerHTML = msg;
+  real_console_log(msg);
+}
